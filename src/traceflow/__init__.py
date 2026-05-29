6e9ef6fe480e63184fb35287853365cb29de0e79
Aarav Agarwal <92234011+firestar3@users.noreply.github.com>
2026-05-28 23:14:46 -0700

    Bump version from 1.2.0 to 1.3.0

--- src/traceflow/__init__.py
+++ src/traceflow/__init__.py
@@ -17,6 +17,6 @@ Global controls:
 
 from traceflow.core import watch, enable, disable, is_enabled
 
-__version__ = "1.2.0"
+__version__ = "1.3.0"
 __author__ = "Aarav Agarwal"
 __all__ = ["watch", "enable", "disable", "is_enabled"]
