75b566943abf122443dc26b6cfd15f46030f9798
Aarav Agarwal <aaravsfo@gmail.com>
2026-05-30 11:18:29 -0700

    v1.5.1: Update to MIT License

--- src/traceflow/__init__.py
+++ src/traceflow/__init__.py
@@ -1,6 +1,6 @@
 
 from traceflow.core import watch, watch_class, enable, disable, is_enabled
 
-__version__ = "1.5.0"
+__version__ = "1.5.1"
 __author__ = "Aarav Agarwal"
 __all__ = ["watch", "watch_class", "enable", "disable", "is_enabled"]
